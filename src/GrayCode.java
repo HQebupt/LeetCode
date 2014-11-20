5c675a0ab4f2d9cec897ef0be2690728c2533e53
HQebupt <qiang.huang1991@gmail.com>
2014-11-20 09:20:06 +0800

    求N位元的格雷码：如何通过二进制转换格雷码.

---
+++ src/GrayCode.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * 本题是求N位元的格雷码。通过二进制数转格雷码的规律公式：
+ * G：格雷码 B：二进制码  , G(N) = (B(n)/2) XOR B(n)
+ * 除以2的运算转换成右移一位的移位操作
+ *
+ */
+public class GrayCode {
+
+	public List<Integer> grayCode(int n) {
+		List<Integer> list = new ArrayList<Integer>(n);
+		int pow = (int) Math.pow(2, n);
+		for (int binary = 0; binary < pow; binary++) {
+			int grayCode = (binary >> 1) ^ binary;
+			list.add(grayCode);
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		GrayCode sol = new GrayCode();
+		int n = 2;
+		System.out.println(sol.grayCode(n));
+	}
+
+}
