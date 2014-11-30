b8bc78475ebd826db29e2dbb11ca318ca9de3979
HQebupt <qiang.huang1991@gmail.com>
2014-11-30 15:17:25 +0800

    PostOrder 迭代用2个栈实现

--- src/BinaryTreePostorderTraversal.java
+++ src/BinaryTreePostorderTraversal.java
@@ -11,28 +11,40 @@ import java.util.Stack;
  *     TreeNode(int x) { val = x; }
  * }
  */
+//Time:O(n) Space:O(n)
 public class BinaryTreePostorderTraversal {
    public List<Integer> postorderTraversal(TreeNode root) {
-		List<Integer> postorder = new ArrayList<Integer>();
+		List<Integer> res = new ArrayList<Integer>();
 		TreeNode p = root;
-		Stack<TreeNode> stack = new Stack<TreeNode>();
+		Stack<TreeNode> s = new Stack<TreeNode>();
 		Stack<Boolean> tag = new Stack<Boolean>();
-		while (p != null || (!stack.isEmpty())) {
+		while (p != null || (!s.isEmpty())) {
 			if (p != null) {
-				stack.push(p);
+				s.push(p);
 				tag.push(false);
 				p = p.left;
 			} else {
 				boolean visit = tag.pop();
 				if (visit) {
-					postorder.add(stack.pop().val);
+					res.add(s.pop().val);
 				} else {
 					tag.push(true);
-					p = stack.peek().right;
+					p = s.peek().right;
 				}
 			}
 		}
-		return postorder;
+		return res;
+	}
+   
+   public static void main(String[] args) {
+		TreeNode t1 = new TreeNode(1);
+		TreeNode t2 = new TreeNode(2);
+		TreeNode t3 = new TreeNode(3);
+		t1.right = t2;
+		t2.left = t3;
+		BinaryTreePostorderTraversal sol = new BinaryTreePostorderTraversal();
+		ArrayList<Integer> res  = (ArrayList<Integer>) sol.postorderTraversal(t1);
+		System.out.println(res);
 	}
 }
 /**
