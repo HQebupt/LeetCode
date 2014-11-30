e8c8f08ccda10d10a8380e3eb70873ed7c1807fb
HQebupt <qiang.huang1991@gmail.com>
2014-11-30 15:16:35 +0800

    preorder 2种实现

--- src/BinaryTreePreorderTraversal.java
+++ src/BinaryTreePreorderTraversal.java
@@ -3,34 +3,47 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for binary tree public class TreeNode { int val; TreeNode left;
+ * TreeNode right; TreeNode(int x) { val = x; } }
  */
 public class BinaryTreePreorderTraversal {
-    List<Integer> preorder = new ArrayList<Integer>();
+	List<Integer> preorder = new ArrayList<Integer>();
 	Stack<TreeNode> stack = new Stack<TreeNode>();
 
-	public List<Integer> preorderTraversal(TreeNode root) {
+	//Sol 1. Time:O(n) Space:O(n)
+	public List<Integer> preorderTraversal1(TreeNode root) {
 		TreeNode p = root;
 		while (p != null || (!stack.isEmpty())) {
-			if(p!=null){
+			if (p != null) {
 				preorder.add(p.val);
 				stack.push(p);
 				p = p.left;
-			}else{
+			} else {
 				p = stack.pop();
 				p = p.right;
 			}
 		}
 		return preorder;
 	}
+	//Sol 2. Time:O(n) Space:O(n)
+	public List<Integer> preorderTraversal2(TreeNode root) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		if (root != null) {
+			s.push(root);
+		}
+		while (!s.isEmpty()) {
+			TreeNode node = s.pop();
+			res.add(node.val);
+			if (node.right != null)
+				s.push(node.right);
+			if (node.left != null)
+				s.push(node.left);
+		}
+		return res;
+	}
 }
 /**
- * solution report:http://www.cnblogs.com/byrhuangqiang/p/3790857.html
- * the blog's end part content.
- */ 
+ * solution report:http://www.cnblogs.com/byrhuangqiang/p/3790857.html the
+ * blog's end part content.
+ */
