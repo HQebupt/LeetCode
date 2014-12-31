693afbbfb7a1098673206ca6597c25a0e0f74234
HQebupt <qiang.huang1991@gmail.com>
2014-12-31 22:48:12 +0800

    Stack,String Split

---
+++ src/SimplifyPath.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class SimplifyPath {
+	public String simplifyPath(String path) {
+		String[] words = path.split("/");
+		ArrayList<String> stack = new ArrayList<String>();
+		for (String s : words) {
+			if (s.equalsIgnoreCase("..")) {
+				if (!stack.isEmpty())
+					stack.remove(stack.size() - 1);
+			} else if (!(s.length() == 0 || s.equalsIgnoreCase("."))) {
+				stack.add(s);
+			}
+		}
+		String res = "";
+		for (String s : stack) {
+			res += "/" + s;
+		}
+		return res.length() == 0 ? "/" : res;
+	}
+
+	public static void main(String[] args) {
+		SimplifyPath sol = new SimplifyPath();
+		String path = "/a/./b/../../c/";
+		System.out.println(sol.simplifyPath(path));
+	}
+}
