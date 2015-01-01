1d8d76a7a92803f18c71b1e9ea8db0451f818622
HQebupt <qiang.huang1991@gmail.com>
2015-01-01 17:28:06 +0800

    fix bug : int will overflow.

--- src/FactorialTrailingZeroes.java
+++ src/FactorialTrailingZeroes.java
@@ -1,23 +1,34 @@
-// insteresting Question: how can I not pass only 2 test in 502 tests ?
+// insteresting Question: how can I not pass only 2 test in 502 tests ? No, I was wrong. Because the i*5 can be overflow for int type.
 // reference: http://www.purplemath.com/modules/factzero.htm
 
 // Time:O(logn) Space:O(1)
 public class FactorialTrailingZeroes {
 	public int trailingZeroes(int n) {
-		if (n == 1808548329)
-			return 452137076;
-		else if (n == 2147483647)
-			return 536870902;
-		
+
 		int count = 0;
-		for (int i = 5; n / i >= 1; i *= 5)
+		for (long i = 5; n / i >= 1; i *= 5)
 			count += n / i;
 		return count;
 	}
 
+	// there is a better solution : not use long type.
+	public int trailingZeroes2(int n) {
+		int res = 0;
+		for (; n > 4; n /= 5) {
+			res += n / 5;
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
 		FactorialTrailingZeroes sol = new FactorialTrailingZeroes();
-		int res = sol.trailingZeroes(1808548329);
+		int res = sol.trailingZeroes(2147483647);
 		System.out.println(res);
 	}
 }
+/*
+ * To avoid the integer overflow as @localvar mentioned below(in case of 'n
+ * >=1808548329' ), the expression " i <= INTMAX/5" is not a good way to prevent
+ * overflow, because 5^13 is > INTMAX/5 and it's valid. So, if you want to use
+ * "multiply", consider define the 'i' as 'long long' type.
+ */
