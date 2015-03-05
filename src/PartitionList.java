84ad2bffac782529468feae98ebf43a96b48b5e5
HQebupt <qiang.huang1991@gmail.com>
2015-03-05 15:10:43 +0800

    format code

--- src/PartitionList.java
+++ src/PartitionList.java
@@ -10,21 +10,24 @@
  * }
  */
 public class PartitionList {
+	
+	// 思路2：dummy1 小于x的linkedlist; dummy2 大于等于x的linkedlist.
+	// Time: O(n)  Space:O(1)
 	public ListNode partition(ListNode head, int x) {
 		ListNode dummy1 = new ListNode(-1);
 		ListNode dummy2 = new ListNode(-2);
-		ListNode t1 = dummy1, t2 = dummy2, pNex = head;
-		while (pNex != null) {
-			ListNode pCur = pNex.next;
-			if (pNex.val < x) {
-				t1.next = pNex;
+		ListNode t1 = dummy1, t2 = dummy2, pCur = head;
+		while (pCur != null) {
+			ListNode pTmp = pCur.next;
+			if (pCur.val < x) {
+				t1.next = pCur;
 				t1 = t1.next;
 			} else {
-				t2.next = pNex;
+				t2.next = pCur;
 				t2 = t2.next;
 				t2.next = null;
 			}
-			pNex = pCur;
+			pCur = pTmp;
 		}
 		t1.next = dummy2.next;
 		return dummy1.next;
