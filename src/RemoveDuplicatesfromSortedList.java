553f9e60b035c11b4aa198afeae2655059923087
HQ <qiang.huang1991@gmail.com>
2015-08-06 17:06:56 +0800

    Two Pointers: 更改while循环为for循环，并增强了健壮性

--- src/RemoveDuplicatesfromSortedList.java
+++ src/RemoveDuplicatesfromSortedList.java
@@ -1,34 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-//时间复杂度:O(n) 空间复杂度O(1)
+// 时间复杂度:O(n) 空间复杂度O(1)
 public class RemoveDuplicatesfromSortedList {
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy = new ListNode(Integer.MAX_VALUE);
-        ListNode tail = dummy;
-        ListNode pCur = head;
-        while(pCur != null){
-        	if(tail.val != pCur.val){
-        		tail.next = pCur;
-        		tail = pCur;
-        	}
-        	pCur = pCur.next;
-        }
-        tail.next = null;
-        return dummy.next;
-    }
-    
-    public static void main(String[] args) {
-    	RemoveDuplicatesfromSortedList sol = new RemoveDuplicatesfromSortedList();
-		ListNode a1 = new ListNode(1);
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode dummy = new ListNode(Integer.MAX_VALUE);
+		ListNode tail = dummy;
+		for (ListNode pCur = head; pCur != null; pCur = pCur.next) {
+			if ((tail.next == null) || (tail.val != pCur.val)) {
+				tail.next = pCur;
+				tail = pCur;
+			}
+		}
+		tail.next = null;
+		return dummy.next;
+	}
+
+	public static void main(String[] args) {
+		RemoveDuplicatesfromSortedList sol = new RemoveDuplicatesfromSortedList();
+		ListNode a1 = new ListNode(Integer.MAX_VALUE);
 		ListNode a2 = new ListNode(1);
 		ListNode a3 = new ListNode(2);
 		ListNode b1 = new ListNode(4);
