00945630f557f74fcec9ae12653692f2d30d96e9
HQ <qiang.huang1991@gmail.com>
2015-08-12 16:25:54 +0800

    卡特兰数

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -2,30 +2,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<String>();
+	public List<String> generateParenthesis(int n) {
+		List<String> result = new ArrayList<String>();
 		String s = "";
-		dfs(n, 0, 0, s, result);
+		dfs2(n, 0, 0, s, result);
 		return result;
-    }
-    // Time:O(n!) Space:O(n)
-    private void dfs(int n, int left, int right, String intermediate, List<String> result) {
-    	if ( left == n && right == n) {
-    		String ans = intermediate.toString();
-    		result.add(ans);
-    		return;
-    	}
-    	if ( left > n || right > n) 
-    		return;
-    	if (left < n) 
-    		dfs(n, left + 1, right, intermediate + "(", result);
-    	if (left - right > 0)
-    		dfs(n, left, right + 1, intermediate + ")", result);
-    }
-    public static void main(String[] args) {
-    	GenerateParentheses sol = new GenerateParentheses();
-    	int n = 3;
-    	List<String> res = sol.generateParenthesis(n);
+	}
+
+	// Time:O(n!) Space:O(n)
+	private void dfs(int n, int left, int right, String cur, List<String> result) {
+		if (left == n && right == n) {
+			String ans = cur.toString();
+			result.add(ans);
+			return;
+		}
+		if (left > n || right > n)
+			return;
+		if (left < n)
+			dfs(n, left + 1, right, cur + "(", result);
+		if (left - right > 0)
+			dfs(n, left, right + 1, cur + ")", result);
+	}
+
+	// 第二次写dfs
+	private void dfs2(int n, int left, int right, String cur,
+			List<String> result) {
+		// 终止条件
+		if (left == n && (right == n)) {
+			result.add(cur);
+			return;
+		}
+
+		// 剪枝
+		if (left < right || (left > n) || (right > n)) {
+			return;
+		}
+
+		// dfs
+		if (left < n) {
+			dfs2(n, left + 1, right, cur + "(", result);
+			// 恢复状态，由于String和int的特殊性，无需额外的代码
+		}
+		if (right < n && (left > right)) {
+			dfs2(n, left, right + 1, cur + ")", result);
+			// 恢复状态，由于String和int的特殊性，无需额外的代码
+		}
+	}
+
+	public static void main(String[] args) {
+		GenerateParentheses sol = new GenerateParentheses();
+		int n = 1;
+		List<String> res = sol.generateParenthesis(n);
+		System.out.println(res.size());
 		System.out.println(res);
 	}
 }
--- src/UniqueBinarySearchTrees.java
+++ src/UniqueBinarySearchTrees.java
@@ -1,22 +1,29 @@
 public class UniqueBinarySearchTrees {
 	// 卡特兰数，一维动态规划，如果用卡特兰公式解的话，时间复杂度可以减少到O(n).
 	// Reference： Code Ganker征服代码
-	// Time:O(n^2)  Space:O(n)
+	// Time:O(n^2) Space:O(n)
 	public int numTrees(int n) {
-		int[] catalan = new int[n + 1];
-		catalan[0] = 1;
-		catalan[1] = 1;
-		for (int i = 2; i < n + 1; i++) {
-			for (int j = 0; j < i; j++) {
-				catalan[i] += catalan[j] * catalan[i - j - 1];
+		int len = n + 1; // 根据结果看，需要catalen[n]的值，需要申请 n + 1的空间。
+		int[] catalan = catalen(len);
+		return catalan[n];
+	}
+
+	// 卡特兰数的模板代码
+	// C[0]-C[5] = {1, 1, 2, 5, 14}
+	private int[] catalen(int len) {
+		int[] cata = new int[len];
+		cata[0] = 1;
+		for (int n = 0; n < len - 1; n++) {
+			for (int i = 0; i <= n; i++) {
+				cata[n + 1] += cata[i] * cata[n - i];
 			}
 		}
-		return catalan[n];
+		return cata;
 	}
 
 	public static void main(String[] args) {
 		UniqueBinarySearchTrees sol = new UniqueBinarySearchTrees();
-		int n = 4;
+		int n = 3;
 		System.out.println(sol.numTrees(n));
 	}
 }
