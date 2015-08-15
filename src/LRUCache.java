21039bed6b94e846da09fc12c8ac60a5b22fbfcb
HQ <qiang.huang1991@gmail.com>
2015-08-15 22:30:56 +0800

    LRU

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -3,34 +3,34 @@ import java.util.LinkedHashMap;
 
 public class LRUCache {
 
-	LinkedHashMap<Integer, Integer> linkedmap;
-	int maxCapacity;
+	LinkedHashMap<Integer, Integer> map;
+	int capacity;
 
 	public LRUCache(int capacity) {
-		this.maxCapacity = capacity;
+		this.capacity = capacity;
 		/*
 		 *int capacity 容量
 		 *int load factor 装载因子
 		 *boolean accessOrder 是否访问顺序排序
 		 */
-		this.linkedmap = new LinkedHashMap<Integer, Integer>(capacity, 1f, true);
+		this.map = new LinkedHashMap<Integer, Integer>(capacity, 1f, true);
 	}
 
 	public int get(int key) {
-		if (linkedmap.containsKey(key))
-			return linkedmap.get(key);
+		if (map.containsKey(key))
+			return map.get(key);
 		else
 			return -1;
 	}
 
 	public void set(int key, int value) {
-		int size = linkedmap.size();
-		if ((size < maxCapacity) || (linkedmap.containsKey(key))) {
-			linkedmap.put(key, value);
-		} else if (size >= maxCapacity) {
-			Iterator<Integer> it = linkedmap.keySet().iterator();
-			linkedmap.remove(it.next());
-			linkedmap.put(key, value);
+		int size = map.size();
+		if ((size < capacity) || (map.containsKey(key))) {
+			map.put(key, value);
+		} else if (size >= capacity) {
+			Iterator<Integer> it = map.keySet().iterator();
+			map.remove(it.next());
+			map.put(key, value);
 		}
 	}
 }
