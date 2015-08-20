10be7c04701060c037aa166f4b4157d6af3eee96
HQ <qiang.huang1991@gmail.com>
2015-08-20 16:45:41 +0800

    add message

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,6 +1,10 @@
 public class ReverseInteger {
 	private static final int MAXDIV10 = Integer.MAX_VALUE / 10;
-
+	/**
+	 * 越界的条件：Math.abs(res) > MAXDIV10
+	 * @param x
+	 * @return
+	 */
 	public int reverse(int x) {
 		int res = 0;
 		while (x != 0) {
@@ -13,8 +17,10 @@ public class ReverseInteger {
 		}
 		return res;
 	}
-
+	
 	public static void main(String[] args) {
+		System.out.println("max:" + Integer.MAX_VALUE);
+		System.out.println("min:" + Integer.MIN_VALUE);
 		ReverseInteger sol = new ReverseInteger();
 		int x = -21448;
 		System.out.println("sol:" + sol.reverse(x));
