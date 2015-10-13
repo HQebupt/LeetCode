100cc28654faff38b89925e5acb847799992d75e
HQ <qiang.huang1991@gmail.com>
2015-10-13 21:52:22 +0800

    链表的归并排序

--- src/SortList.java
+++ src/SortList.java
@@ -9,30 +9,55 @@
  *     }
  * }
  */
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 public class SortList {
+	
+	// 链表的堆排序:很基本的题目，要熟练掌握。
+	// 时间复杂度 O(nlogn) 空间复杂度,由于堆排序这儿压栈了，应该不是常量的空间，是logn的空间。
     public ListNode sortList(ListNode head) {
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		ListNode p = head;
-		while (p != null) {
-			arr.add(p.val);
-			p = p.next;
-		}
-		Collections.sort(arr);
-		Iterator<Integer> it = arr.iterator();
-		p = head;
-		while (it.hasNext() && p != null) {
-			p.val = it.next();
-			p = p.next;
-		}
-		return head;
+    	if(head == null || head.next == null)  
+            return head;  
+    	ListNode slow = head, fast = head;
+    	while(fast.next != null && fast.next.next != null) {
+    		slow = slow.next;
+    		fast = fast.next.next;
+    	}
+    	ListNode head2 = slow.next;
+    	slow.next = null;
+    	ListNode l1 = sortList(head);
+    	ListNode l2 = sortList(head2);
+    	return merge(l1,l2);
 	}
+
+	private ListNode merge(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(-1);
+		ListNode tail = dummy;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				tail.next = l1;
+				l1 = l1.next;
+			} else {
+				tail.next = l2;
+				l2 = l2.next;
+			}
+			tail = tail.next;
+ 		}
+		tail.next = l1 == null ? l2 : l1;
+		return dummy.next;
+	}
+	
+	public static void main(String[] args) {
+		SortList sol = new SortList();
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(3);
+		ListNode l4= new ListNode(4);
+		ListNode l5 = new ListNode(5);
+		l5.next = l4;
+		l4.next = l3;
+		l3.next = l2;
+		l2.next = l1;
+		System.out.println(sol.sortList(l5));
+		
+	}
+	
 }
-/**
- * get all the linked chain's values ,and store into ArrayList;
- * use the Collections.sort() to sort.And fill  the sorted values into the linked chain.
- * PS:Collections.sort's implement is Arrays.sort(), more deeply, is MergeSort(nlog(n) timeComplex).
- * Attentions:I do not make out the mergeSort, Need to implement all kinds of sort algorthoms by myself.
- */ 
