d10f1edd49358c1c0a73ddb5ae3f5257e13c5b8f
HQebupt <qiang.huang1991@gmail.com>
2018-11-21 15:23:05 +0800

    2nd commit: 2nd method

--- src/main/java/BinaryTreeInorderTraversal.java
+++ src/main/java/BinaryTreeInorderTraversal.java
@@ -17,15 +17,23 @@ public class BinaryTreeInorderTraversal {
         Stack<TreeNode> s = new Stack<TreeNode>();
         ArrayList<Integer> res = new ArrayList<Integer>();
         TreeNode p = root;
-        while (p != null || !s.isEmpty()) {
-        	if (p != null) {
-        		s.push(p);
-        		p = p.left;
-        	}else{
-        		p = s.pop();
-        		res.add(p.val);
-        		p = p.right;
-        	}
+       	if (p != null) {
+       	    s.push(p);
+        }
+        while(!s.isEmpty()) {
+            if (p == null || p.left == null) {
+                p = s.pop();
+                res.add(p.val);
+                if (p.right != null) {
+                    s.push(p.right);
+                    p = p.right;
+                } else {
+                    p = null;
+                }
+            } else if(p.left != null) {
+                s.push(p.left);
+                p = p.left;
+            }
         }
         return res;
     }
